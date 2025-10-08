f16054ae71d4b503e731468227f5116ba485337d
fayymar <marlen.yuldashev@gmail.com>
2025-10-08 11:36:59 +0500

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,3 @@
-# bot/__init__.py
-"""Telegram bot package"""
-
-# bot/handlers/__init__.py
-"""Bot handlers package"""
-
-# database/__init__.py
-"""Database package"""
-
-# services/__init__.py
-"""Services package"""
+"""
+Bot module for Telegram Medical Bot
+"""
