0f7a92fb9b28a1a3e0c2aa26b4fd903599cf55d6
fayymar <marlen.yuldashev@gmail.com>
2025-10-08 11:18:29 +0500

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -1,8 +1,8 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class RegistrationStates(StatesGroup):
-    """Состояния регистрации пользователя"""
+class Registration(StatesGroup):
+    """Состояния для регистрации пользователя"""
     waiting_for_full_name = State()
     waiting_for_phone = State()
     waiting_for_birthdate = State()
@@ -11,22 +11,35 @@ class RegistrationStates(StatesGroup):
     waiting_for_weight = State()
 
 
-class ConsultationStates(StatesGroup):
-    """Состояния консультации - новая структура"""
-    # Этап 1: Описание симптомов
+class EditProfile(StatesGroup):
+    """Состояния для редактирования профиля"""
+    choosing_field = State()  # Выбор поля для редактирования
+    waiting_for_full_name = State()
+    waiting_for_phone = State()
+    waiting_for_birthdate = State()
+    waiting_for_gender = State()
+    waiting_for_height = State()
+    waiting_for_weight = State()
+
+
+class Consultation(StatesGroup):
+    """Состояния для консультации"""
+    # Этап 1: Основные симптомы
     waiting_for_symptoms = State()
     confirming_symptoms = State()
-    adding_symptoms = State()
     
-    # Этап 2: Давность
-    selecting_duration = State()
+    # Этап 2: Давность симптомов
+    waiting_for_duration = State()
     
-    # Этап 3: Дополнительные симптомы от AI
+    # Этап 3: Дополнительные симптомы
     selecting_additional_symptoms = State()
-    entering_custom_symptoms = State()
+    waiting_for_other_symptoms = State()
     
     # Этап 4: Финальное подтверждение
     final_confirmation = State()
-    
-    # Результат
-    viewing_result = State()
+
+
+class FindSpecialist(StatesGroup):
+    """Состояния для поиска специалиста"""
+    choosing_category = State()
+    viewing_specialists = State()
